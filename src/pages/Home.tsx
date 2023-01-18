2e78e2ac4534dc5482ee5c69ac8cddf4e529b0cf
Eadweard <804875728@qq.com>
2023-01-18 22:24:45 +0800

    动画提交

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import 'styles/home.scss'
 function Home() {
   return (
     <main className='h-full w-full flex justify-center p-t-8'>
-      <div className='h-100% w-90% flex justify-around'>
-        <div className='h-100% w-45%'>
+      <div className='h-100% w-70% flex justify-around'>
+        <article>
           <h2>Name</h2>
           <p>Hey, I am Anthony Fu, a fanatical open sourceror. Working at NuxtLabs.
 
@@ -13,8 +13,8 @@ function Home() {
             Team member of Windi CSS, wenyan-lang 文言 and Intlify.
 
             Dreaming up ideas and making them come true is where my passion lies. You can find my full projects list here. I have also started a 100-day challenge about design, compform, interactivity, and experimenting with new things on 100.antfu.me.</p>
-        </div>
-        <div className='h-100% w-45%'>
+        </article>
+        <div className='right_img'>
           <img src="/images/blog_index.jpg" className='w-full h-600px' alt="" />
         </div>
       </div>
